77b8d17ae3168f5851011a7cc8d07e524b37a2c2
mooali01 <mooali01@gmail.com>
2020-01-09 22:20:26 -0600

    Add files via upload
    class-4

---
+++ coding.js
@@ -0,0 +1,27 @@
+function myFirst() {
+    console.log("Muhammad");
+}
+​
+function mySecond() {
+    console.log("Ali");
+}
+​
+​
+function myFirstwarn() {
+    console.warn("Muhammad");
+}
+​
+function mySecondwarn() {
+    console.warn("Ali");
+}
+​
+function myFirstinfo() {
+    console.info("Muhammad");
+}
+​
+​
+function printFullName() {
+    alert("Adam Sam Brian");
+    console.log("Adam Sam Brian");
+}
+
